c6a9766862a6a9f105bee712f7cc73aa7066d1c4
yet-another-arch-user <dgthinh@proton.me>
2026-07-22 14:01:48 +0700

    Changed Hyprland spring animation for 0.56 update

--- .config/hypr/modules/animation.lua
+++ .config/hypr/modules/animation.lua
@@ -12,7 +12,7 @@ hl.curve("quick",          { type = "bezier", points = { {0.15, 0},    {0.1, 1} 
 
 -- Default springs
 -- hl.curve("easy",           { type = "spring", mass = 1, stiffness = 71.2633, dampening = 15.8273644 })
-hl.curve("easy",           { type = "spring", mass = 1, stiffness = 100, dampening = 20 })
+hl.curve("easy",           { type = "spring", mass = 0.7, stiffness = 200, dampening = 20 })
 
 hl.animation({ leaf = "global",        enabled = true,  speed = 10,   bezier = "default" })
 hl.animation({ leaf = "border",        enabled = true,  speed = 5.39, bezier = "easeOutQuint" })
