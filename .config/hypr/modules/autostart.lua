5d43b81b8ab750a471a7176ee018c194965810aa
yet-another-arch-user <dgthinh@proton.me>
2026-08-24 15:20:30 +0700

    Too lazy for commit message

--- .config/hypr/modules/autostart.lua
+++ .config/hypr/modules/autostart.lua
@@ -7,6 +7,5 @@
 -- Or execute your favorite apps at launch like this:
 --
 hl.on("hyprland.start", function () 
-  hl.exec_cmd("matugen image $HOME/.cache/wallpaper --source-color-index 0 || matugen image $HOME/.config/wallpaper.jpg --source-color-index 0")
-  hl.exec_cmd("waybar")
+  hl.exec_cmd("matugen image $HOME/.cache/wallpaper --source-color-index 0 && waybar || matugen image $HOME/.config/wallpaper.jpg --source-color-index 0 && waybar")
 end)
--- .config/hypr/modules/colors.lua
+++
@@ -1,4 +0,0 @@
-return {
-	border_active = "#9fcafc",
-	border_inactive = "#003257"
-}
